0b02c6dae15e26acc5a7f630192e31ab66dfe629
SklibaLibus <trojakovakristina@gmail.com>
2020-01-28 19:11:40 +0100

    add divide by zero exam

---
+++ week-08-s/DivideByZero/DivideByZero/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            try
+            {
+                Console.Write("Input a number: ");
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{ a }/{ b }" + " = " + a / b);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("fail, attempt divided by zero");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("wrong input");
+            }
+        }
+    }
+}
