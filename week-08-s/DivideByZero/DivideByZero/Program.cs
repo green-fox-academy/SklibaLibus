b17648aa6a7eaa045cc24353a57384b136fc468a
SklibaLibus <trojakovakristina@gmail.com>
2020-01-28 19:25:47 +0100

    create divide by zero function with while

--- week-08-s/DivideByZero/DivideByZero/Program.cs
+++ week-08-s/DivideByZero/DivideByZero/Program.cs
@@ -7,21 +7,49 @@ namespace DivideByZero
         static void Main(string[] args)
         {
 
-            int a = 10;
-            try
+            //    int a = 10;
+            //    try
+            //    {
+            //        Console.Write("Input a number: ");
+            //        int b = int.Parse(Console.ReadLine());
+            //        Console.WriteLine($"{ a }/{ b }" + " = " + a / b);
+            //    }
+            //    catch (DivideByZeroException)
+            //    {
+            //        Console.WriteLine("fail, attempt divided by zero");
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("wrong input");
+            //    }
+            //}
+            DivideByZero();
+        }
+        static void DivideByZero()
+        {
+            int divider = 10;
+            int userNumberInput;
+            int result;
+
+            while (true)
             {
-                Console.Write("Input a number: ");
-                int b = int.Parse(Console.ReadLine());
-                Console.WriteLine($"{ a }/{ b }" + " = " + a / b);
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("fail, attempt divided by zero");
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("wrong input");
+                try
+                {
+                    Console.Write("Please enter a number: ");
+                    userNumberInput = Int32.Parse(Console.ReadLine());
+                    result = divider / userNumberInput;
+                    Console.WriteLine(result);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("fail, attempt divided by zero");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("wrong input");
+                }
             }
         }
     }
 }
+
