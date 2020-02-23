517107f7d0df8eaa589963d015820146941d0e83
Kristina <trojakovakristina@gmail.com>
2020-02-23 16:55:46 +0100

    Add Cloneable exercise

---
+++ week-12-s/Cloneable/Cloneable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cloneable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student john = new Student("John Doe", 20, "male", "BME", 8);
+            Student jane = new Student("Jane Nondoe", 25, "female", "Microsoft");
+            Student student = new Student();
+
+            jane.Introduce();
+
+            Student JohnTheClone = (Student) john.Clone();
+            JohnTheClone.Introduce();
+
+            Student Clone = (Student) student.Clone();
+            Clone.Introduce();
+        }
+    }
+}
---
+++ week-12-s/Cloneable/Cloneable/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloneable
+{
+    class Student : Person, ICloneable
+    {
+        public string PreviousOrganization { get; set; }
+        public int SkippedDays { get; set; }
+
+        public Student(string name, int age, string gender, string previousOrganization, int skippedDays = 0) : base(name, age, gender)
+        {
+            PreviousOrganization = previousOrganization;
+            SkippedDays = skippedDays;
+        }
+
+        public Student()
+        {
+            Name = "Free Mind";
+            Age = 30;
+            Gender = "unknown gender";
+            PreviousOrganization = "The School of Life";
+        }
+
+        public override void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, a {Age} year old {Gender} from {PreviousOrganization} who skipped {SkippedDays} days from the course already.");
+        }
+
+        public override void GetGoal()
+        {
+            Console.WriteLine("My goal is: Be a junior software developer.");
+        }
+
+        public void SkipDays(int numberOfDays)
+        {
+            SkippedDays += numberOfDays;
+        }
+
+        public object Clone()
+        {
+            return new Student(Name, Age, Gender, PreviousOrganization, SkippedDays);
+        }
+    }
+}
